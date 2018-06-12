b70065fe5d9be30769e8832ef0f2644c9a569f90
Tommaso Allevi <tomallevi@gmail.com>
2018-06-13 01:24:28 +0200

    Fix node6 #2

--- test.js
+++ test.js
@@ -46,36 +46,39 @@ t.test('fastify-oauth2', t => {
       })
   })
 
-  t.test('ok', async t => {
+  t.test('ok', t => {
     t.plan(4)
 
-    const responseStart = await fastify.inject({
+    return fastify.inject({
       method: 'GET',
       url: '/login/github'
     })
+      .then(responseStart => {
+        t.equal(responseStart.statusCode, 302)
+        t.equal(responseStart.headers.location, 'https://github.com/login/oauth/authorize?response_type=code&client_id=my-client-id&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=notifications&state=3(%230%2F!~')
 
-    t.equal(responseStart.statusCode, 302)
-    t.equal(responseStart.headers.location, 'https://github.com/login/oauth/authorize?response_type=code&client_id=my-client-id&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fcallback&scope=notifications&state=3(%230%2F!~')
+        const RESPONSE_BODY = {
+          access_token: 'my-access-token',
+          refresh_token: 'my-refresh-token',
+          token_type: 'bearer',
+          expires_in: '240000'
+        }
 
-    const RESPONSE_BODY = {
-      access_token: 'my-access-token',
-      refresh_token: 'my-refresh-token',
-      token_type: 'bearer',
-      expires_in: '240000'
-    }
-    const githubScope = nock('https://github.com')
-      .post('/login/oauth/access_token', 'code=my-code&grant_type=authorization_code&client_id=my-client-id&client_secret=my-secret')
-      .reply(200, RESPONSE_BODY)
+        const githubScope = nock('https://github.com')
+          .post('/login/oauth/access_token', 'code=my-code&grant_type=authorization_code&client_id=my-client-id&client_secret=my-secret')
+          .reply(200, RESPONSE_BODY)
 
-    const responseEnd = await fastify.inject({
-      method: 'GET',
-      url: '/?code=my-code'
-    })
+        return fastify.inject({
+          method: 'GET',
+          url: '/?code=my-code'
+        })
+          .then(responseEnd => {
+            t.equal(responseEnd.statusCode, 200)
+            t.strictSame(JSON.parse(responseEnd.payload), RESPONSE_BODY)
 
-    t.equal(responseEnd.statusCode, 200)
-    t.strictSame(JSON.parse(responseEnd.payload), RESPONSE_BODY)
-
-    githubScope.done()
+            githubScope.done()
+          })
+      })
   })
 
   t.end()
