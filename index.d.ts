3306d9b52a5c217274a88010b42d5aade6470b35
Peter Balazs <frikille@gmail.com>
2020-05-01 13:47:40 +0100

    update fastify to v3

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,63 @@
-import * as fastify from 'fastify';
+import fastify, { FastifyPlugin } from 'fastify';
 import * as http from 'http';
 
-declare function fastifyOauth2(
-  instance: fastify.FastifyInstance<http.Server, http.IncomingMessage, http.ServerResponse>,
-  options: fastifyOauth2.FastifyOAuth2Options,
-  callback: (err?: fastify.FastifyError) => void,
-): void;
+export interface OAuth2Token{
+  token_type: 'bearer';
+  access_token: string;
+  refresh_token?: string;
+  expires_in: number
+}
+
+export interface OAuth2Namespace{
+  getAccessTokenFromAuthorizationCodeFlow(
+    request: fastify.FastifyRequest<http.IncomingMessage>, 
+  ): Promise<OAuth2Token>
+
+  getAccessTokenFromAuthorizationCodeFlow(
+    request: fastify.FastifyRequest<http.IncomingMessage>, 
+    callback: (token: OAuth2Token) => void
+  ): void
+
+  getNewAccessTokenUsingRefreshToken(
+    refreshToken: string, 
+    params: Object, 
+    callback: (token: OAuth2Token) => void
+  ): void
+
+  getNewAccessTokenUsingRefreshToken(
+    refreshToken: string, 
+    params: Object, 
+  ): Promise<OAuth2Token>
+}
+
+export interface ProviderConfiguration {
+  authorizeHost: string;
+  authorizePath: string;
+  tokenHost: string;
+  tokenPath: string;
+}
+
+export interface Credentials {
+  client: {
+    id: string;
+    secret: string;
+  };
+  auth: ProviderConfiguration;
+}
+
+export interface FastifyOAuth2Options {
+  name: string;
+  scope: string[];
+  credentials: Credentials;
+  callbackUri: string;
+  callbackUriParams?: Object;
+  generateStateFunction?: Function;
+  checkStateFunction?: Function;
+  startRedirectPath: string;
+}
+
+
+declare const fastifyOauth2: FastifyPlugin<FastifyOAuth2Options>
 
 declare namespace fastifyOauth2 {
   const FACEBOOK_CONFIGURATION: ProviderConfiguration;
