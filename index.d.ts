af1d0bd58c9c032afb7adea9788789873dcf0625
Marcus Naughton <3456318+LKNSI@users.noreply.github.com>
2021-12-16 16:37:56 -0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -10,6 +10,7 @@ interface FastifyOAuth2 {
   MICROSOFT_CONFIGURATION: ProviderConfiguration;
   SPOTIFY_CONFIGURATION: ProviderConfiguration;
   VKONTAKTE_CONFIGURATION: ProviderConfiguration;
+  TWITCH_CONFIGURATIONS: ProviderConfiguration;
 }
 
 export const fastifyOauth2: FastifyPlugin<FastifyOAuth2Options> & FastifyOAuth2
