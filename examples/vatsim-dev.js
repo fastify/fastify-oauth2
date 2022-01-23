d15eab5826f986c6b39f0f86c4b57e44723a8f22
Enrico1108 <55299725+Enrico1108@users.noreply.github.com>
2022-01-23 11:11:38 -0300

    Create vatsim-dev.js

---
+++ examples/vatsim-dev.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const fastify = require('fastify')({ logger: { level: 'trace' } })
+const sget = require('simple-get')
+
+// const oauthPlugin = require('fastify-oauth2')
+const oauthPlugin = require('..')
+
+fastify.register(oauthPlugin, {
+  name: 'vatsimoauthdev',
+  scope: ['full_name', 'email', 'vatsim_details', 'country'],
+  credentials: {
+    client: {
+      id: '<CLIENT_ID>',
+      secret: '<CLIENT_SECRET>'
+    },
+    auth: oauthPlugin.VATSIM_DEV_CONFIGURATION
+  },
+  startRedirectPath: '/login/vatsim',
+  callbackUri: 'http://localhost:3000/login/vatsim/callback'
+})
+
+fastify.get('/login/vatsim/callback', function (request, reply) {
+  this.vatsimoauthdev.getAccessTokenFromAuthorizationCodeFlow(
+    request,
+    (err, result) => {
+      if (err) {
+        reply.send(err)
+        return
+      }
+
+      sget.concat(
+        {
+          url: 'https://auth-dev.vatsim.net/api/user',
+          method: 'GET',
+          headers: {
+            Authorization: 'Bearer ' + result.access_token
+          },
+          json: true
+        },
+        function (err, res, data) {
+          if (err) {
+            reply.send(err)
+            return
+          }
+          reply.send(data)
+        }
+      )
+    }
+  )
+})
+
+fastify.listen(3000)
