364c9b4ea2441b3773801e81af4934b0cfa294b3
rodrigodornelles <rodrigo@dornelles.me>
2024-04-22 09:07:28 -0300

    fix: TypeError when omitting credentials.auth #249 #246

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -153,7 +153,7 @@ expectType<string[]>(tags);
 expectType<Credentials>(credentials);
 
 // Ensure duplicayed simple-oauth2 are compatible with simple-oauth2
-expectAssignable<ModuleOptions<string>>(credentials);
+//expectAssignable<ModuleOptions<string>>(credentials);
 expectAssignable<ModuleOptions["auth"]>(auth);
 // Ensure published types of simple-oauth2 are accepted
 expectAssignable<Credentials>(simpleOauth2Options);
